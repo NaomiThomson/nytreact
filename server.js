f27e6d37532ec6a6ba388be38a8f70d2892af506
Naomi Thomson <naomithomson94@gmail.com>
2017-08-07 19:45:15 -0700

    mongoose connection

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var Article= require("./models/article");
+var mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,9 +13,22 @@ app.use(function (req, res, next) {
   } else {
     next();
   }
-})
+});
 
 app.use(express.static('public'));
+
+// -------------------------------------------------
+// MongoDB configuration (Change this URL to your own DB)
+mongoose.connect("mongodb://heroku_wbb09z7q:p5om9dq0v4v98eqvq83tc7daia@ds149201.mlab.com:49201/heroku_wbb09z7q");
+var db = mongoose.connection;
+db.on("error", function (err) {
+  console.log("Mongoose Error: ", err);
+});
+db.once("open", function () {
+  console.log("Mongoose connection successful.");
+});
+// -------------------------------------------------
+
 
 app.listen(PORT, function () {
   console.log('Express server is up on port 3000');
